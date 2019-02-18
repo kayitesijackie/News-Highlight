a4d5773e900bce796db65949b388ab9c5df4d73b
kayitesijackie <bazimyas@gmail.com>
2019-02-18 12:26:30 +0200

    add get_sources function that gets json response to our url request

--- app/requests.py
+++ app/requests.py
@@ -13,4 +13,20 @@ def configure_request(app):
     source_base_url = app.config['NEWS_SOURCES_BASE_URL']
     articles_base_url = app.config['NEWS_ARTICLES_API_BASE_URL']
 
+def get_sources(category):
+    '''Function that gets json response to our url request'''
+    get_sources_url = source_base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
+
 #....
