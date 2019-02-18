0b678ad1dcf8b51b6539f5107ac9163a2c554478
kayitesijackie <bazimyas@gmail.com>
2019-02-18 12:13:23 +0200

    author attribute in Article class

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class Article:
     def __init__(self,id,title,author,description,url,urlToImage,publishedAt,content):
         self.id = id
         self.title = title
-        self.author
+        self.author= author
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
