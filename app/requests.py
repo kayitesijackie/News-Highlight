1e946495760d3c85b05ad24008feb41331abdc07
kayitesijackie <bazimyas@gmail.com>
2019-02-19 10:30:05 +0200

    import app module

--- app/requests.py
+++ app/requests.py
@@ -1,21 +1,23 @@
 import urllib.request,json
 from .models import Source,Article
+from app import app
 
 # Getting api key
-api_key = None
-# Getting the news base url
-source_base_url = None
-articles_base_url = None
+# api_key = None
+# # Getting the news base url
+# source_base_url = None
+# articles_base_url = None
 
-def configure_request(app):
-    global api_key,source_base_url,articles_base_url
-    api_key = app.config['NEWS_API_KEY']
-    source_base_url = app.config['NEWS_SOURCES_BASE_URL']
-    articles_base_url = app.config['NEWS_ARTICLES_API_BASE_URL']
+# def configure_request(app):
+#     global api_key,source_base_url,articles_base_url
+api_key = app.config['NEWS_API_KEY']
+source_base_url = app.config['NEWS_SOURCES_BASE_URL']
+articles_base_url = app.config['NEWS_ARTICLES_API_BASE_URL']
 
 def get_sources(category):
     '''Function that gets json response to our url request'''
     get_sources_url = source_base_url.format(category,api_key)
+    
 
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
